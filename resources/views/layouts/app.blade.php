2b5f8c9bfab648cfc226a84b6fd88d6c2a6937db
Mohammed Omar <omarmd2311@gmail.com>
2020-06-13 13:32:43 +0530

    added css and js files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<title>City Classified Search</title>
+	@include('includes.head')
+</head>
+<body>	
+@include('includes.navbar')
+@yield('css')
+@yield('content')
+@include('includes.footer')
+@yield('js')
+</body>
+</html>
